a32a18f8a7f99c2bf1f6cbdb57051ad7f15119d3
Itai <62836633+IDotan@users.noreply.github.com>
2021-10-26 11:29:31 +0300

    Add files via upload

--- ex5/static/scripts/scripts.js
+++ ex5/static/scripts/scripts.js
@@ -1,18 +1,26 @@
 let nav_mousedown_m = 0;
 
+/**
+ * Toggle class 'disable_scroll' when mobile menu open or close
+ */
 function scrollControl() {
-    // toggle class 'disable_scroll' when mobile menu open or close
     document.getElementsByTagName('body')[0].classList.toggle('disable_scroll');
 };
 
+/**
+ * Close mobile menu, un check the checkbox and enable scrolling.
+ */
 function mobile_menu_close() {
-    // close mobile menu, un check the checkbox and enable scrolling
     document.getElementById('menu_toggle').checked = false;
     document.getElementsByTagName('body')[0].classList.remove('disable_scroll');
 };
 
+/**
+ * When click/touch down in mobile menu get the start coordinate.
+ * 
+ * @param {Object} event event object from the listener
+ */
 function nav_swip_down(event) {
-    // when click/touch down in mobile menu get the start coordinate
     if (event.type == 'mousedown') {
         nav_mousedown_m = event.clientX;
     } else {
@@ -20,8 +28,12 @@ function nav_swip_down(event) {
     };
 };
 
+/**
+ * When click/touch down in mobile menu and moved to the left close the menu.
+ * 
+ * @param {Object} event event object from the listener
+ */
 function nav_swip_up(event) {
-    // when click/touch down in mobile menu and moved to the left close the menu
     if (event.type == 'mouseup') {
         if (event.clientX < nav_mousedown_m) {
             mobile_menu_close();
@@ -33,28 +45,53 @@ function nav_swip_up(event) {
     };
 };
 
+/**
+ * Event listener to close the mobile menu when a link is clicked.
+ */
 function navLinksEvents() {
-    // event listener to close the mobile menu when a link is clicked
     let links = document.getElementsByTagName('nav')[0].children;
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener('click', mobile_menu_close);
     };
 };
 
+/**
+ * Event listeners to close mobile menu when draging left.
+ */
 function mobile_nav_close_listener() {
-    // event listeners to close mobile menu when draging left
-    document.getElementsByTagName('nav')[0].addEventListener('mousedown', nav_swip_down, true);
-    document.getElementsByTagName('nav')[0].addEventListener('touchstart', nav_swip_down, true);
-    document.getElementsByTagName('nav')[0].addEventListener('mouseup', nav_swip_up, true);
-    document.getElementsByTagName('nav')[0].addEventListener('touchend', nav_swip_up, true);
+    document.getElementsByTagName('nav')[0].addEventListener('mousedown', nav_swip_down);
+    document.getElementsByTagName('nav')[0].addEventListener('touchstart', nav_swip_down);
+    document.getElementsByTagName('nav')[0].addEventListener('mouseup', nav_swip_up);
+    document.getElementsByTagName('nav')[0].addEventListener('touchend', nav_swip_up);
 };
 
+/**
+ * Switch display from python to web projects.
+ */
+function project_tab_switch() {
+    let tab = this.getElementsByTagName('span')[0].innerText;
+    let folder = document.getElementById('projects_folder');
+    document.querySelectorAll('.projects_tab').forEach((tab) => { tab.classList.remove('sunken_tab') });
+    if (tab == "Python projects") {
+        folder.className = "python_tab";
+        document.getElementsByClassName('projects_tab web_tab')[0].classList.add('sunken_tab');
+    } else if (tab == "Web projects") {
+        folder.className = "web_tab";
+        document.getElementsByClassName('projects_tab python_tab')[0].classList.add('sunken_tab');
+    };
+}
+
+/**
+ * Assaign all event listeners for the page.
+ */
 function eventAssign() {
-    // assaign all event listeners for the page
-    window.addEventListener('resize', mobile_menu_close)
+    window.addEventListener('resize', mobile_menu_close);
     document.getElementsByClassName('menu_btn')[0].addEventListener('click', scrollControl);
     navLinksEvents();
     mobile_nav_close_listener();
+    document.querySelectorAll('.projects_tab').forEach((tab) => {
+        tab.addEventListener('click', project_tab_switch);
+    });
 };
 
 eventAssign();
